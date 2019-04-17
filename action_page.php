2137b231459220157a83e6828c2e112ddcf1b2a5
Vernon Williams <voxenn@gmail.com>
2019-04-16 21:04:12 -0500

    Version 2.0

---
+++ action_page.php
@@ -0,0 +1,11 @@
+<?php
+$firstnamename = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$formcontent="From: $name \n Message: $message";
+$recipient = "vernonvwilliams@gmail.com";
+$subject = "Contact Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!";
+?>
